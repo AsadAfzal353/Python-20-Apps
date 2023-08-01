c3397d630125c903bc0b8bf5c0330aaee79b4311
AsadAfzal353 <asadafzal353@gmail.com>
2023-08-01 20:11:24 +0500

    App 4 Done

--- app4-invoicegen/challenge/main.py
+++ app4-invoicegen/challenge/main.py
@@ -10,6 +10,14 @@ pdf.set_font(family="Times", size=24, style="B")
 for filepath in filepaths:
     filename = Path(filepath).stem
     pdf.add_page()
-    pdf.cell(w=50, h=24, txt=filename.title())
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=24, txt=filename.title(), ln=1)
+
+    with open(filepath, "r") as file:
+        content = file.read()
+
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 pdf.output(f"app4-invoicegen/challenge/output.pdf")
--- app4-invoicegen/main.py
+++ app4-invoicegen/main.py
@@ -7,11 +7,61 @@ filepaths = glob.glob("app4-invoicegen/invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
+    
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Add a header
+    columns = list(df.columns)
+    columns = [column.replace("_", " ").title() for column in columns]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=str(columns[0]), border=1)
+    pdf.cell(w=70, h=8, txt=str(columns[1]), border=1)
+    pdf.cell(w=30, h=8, txt=str(columns[2]), border=1)
+    pdf.cell(w=30, h=8, txt=str(columns[3]), border=1)
+    pdf.cell(w=30, h=8, txt=str(columns[4]), border=1, ln=1)    
+
+    # Other rows
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+    
+    # Performing the sum
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Adding total sum sentence
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+    # Company name and logo
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=30, h=8, txt=f"PythonHow")
+    pdf.image("app4-invoicegen/pythonhow.png", w=10)
+
+
     pdf.output(f"app4-invoicegen/PDFs/{filename}.pdf")
     
