acb95b18555df8aa68ae0891c4f7cffff27e1a6b
AsadAfzal353 <asadafzal353@gmail.com>
2023-08-02 12:19:17 +0500

    app5 done

--- app5-newsapi/challenge2/main.py
+++ app5-newsapi/challenge2/main.py
@@ -4,23 +4,15 @@ import streamlit as st
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
-with col1:
-    st.title = "NASA1"
+api_key= "oP7Ao9y1eHS6hyevyHaEon1qgAtM4i0WxxvkBqeJ"
+url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
-with col2:
-    st.title = "NASA2"
+request = requests.get(url)
 
+content = request.json()
 
-# api_key= "oP7Ao9y1eHS6hyevyHaEon1qgAtM4i0WxxvkBqeJ"
-# url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+print(content)
+st.header(content["title"])
+st.image(content["url"])
+st.write(content["explanation"])
 
-# request = requests.get(url)
-
-# content = request.json()
-
-# print(content)
-# st.header = content["title"]
-# st.image = content["hdurl"]
-# st.write = content["explanation"]
-
-st.info = "NASA"
